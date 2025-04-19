34ddaf6a909058324781b9581404b970ac692e6a
Marioc9955 <marioc9955@gmail.com>
2025-04-19 11:51:30 -0500

    Con x2

--- main.ts
+++ main.ts
@@ -38,7 +38,7 @@ router.post("/teapot", (ctx) => {
 
 function getFYByTwoPoints(x1: number, y1: number, x2: number, y2: number): (x: number) => number {
     const m = (y2 - y1) / (x2 - x1);
-    const b = y1 - m * x1;
+    const b = y2 - m * x2;
     return (y: number) => (y - b) / m;
 }
 
